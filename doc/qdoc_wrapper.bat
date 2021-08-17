d22c366ac9bd505cf72dc0e6c480864e6b5bbf76
liangzuoting <liangzuoting@gmail.com>
2021-08-17 12:31:24 +0800

    static release build

--- doc/qdoc_wrapper.bat
+++ doc/qdoc_wrapper.bat
@@ -3,7 +3,7 @@ SetLocal EnableDelayedExpansion
 (set QT_VERSION=4.0.1)
 (set QT_VER=4.0)
 (set QT_VERSION_TAG=401)
-(set QT_INSTALL_DOCS=D:/QT/Docs/Qt-5.15.0)
+(set QT_INSTALL_DOCS=G:/Qt/Static/5.15.2/doc)
 (set BUILDDIR=E:/installer-framework/doc)
-D:\QT\5.15.0\MSVC2019_64\bin\qdoc.exe %*
+G:\Qt\Static\5.15.2\bin\qdoc.exe %*
 EndLocal
